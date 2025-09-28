ceed80bf70f946dbd0079689842bb9bc02817404
sirius <916108538@qq.com>
2025-09-28 11:26:32 +0800

    ag-layout-demo base version

---
+++ aggen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+protoc --proto_path=./idl \
+    --go_out=paths=source_relative:./ \
+    --proto_path=./third_party \
+    --openapi_out=fq_schema_naming=true,default_response=false:. \
+    --go-agserver_out=paths=source_relative:./api \
+    --go-aghertz_out=paths=source_relative:./api \
+    --go-agkitex_out=paths=source_relative:./api \
+    --go-agservice_out=paths=source_relative:./api \
+    idl/api/**/*.proto
