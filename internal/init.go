2f994a029192b615f5a734af9251abb0f3ffb920
sirius <916108538@qq.com>
2026-01-29 09:17:45 +0800

    base init

---
+++ internal/init.go
@@ -0,0 +1,16 @@
+package internal
+
+import (
+	"ag-core/ag/ag_common/agmetadata"
+	"ag-core/contribute/agdb"
+	"ag-layout-demo/internal/svcgen"
+)
+
+func init() {
+	// 初始化headdev
+	agmetadata.RegMdKey("ORG")
+	agmetadata.RegMdKey("Tradeid")
+
+	// 注册事务
+	agdb.TransactionRegistry.RegTxForCall(svcgen.StudentServiceCreateStudentCallInfo)
+}
